c5255f031ad37012a4ce435c7944819a937ae9d8
Reo Matsuda <reo.matsuda@gmail.com>
2022-02-16 10:19:47 -0600

    Server index cleanup

--- server/index.js
+++ server/index.js
@@ -36,20 +36,3 @@ app.get("/", async (req, res) => {
             }
         });
 });
-
-// const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
-
-// app.use(
-//     cors({
-//         origin: function(origin, callback) {
-//             if (!origin) return callback(null, true);
-//             if (allowedOrigins.indexOf(origin) === -1) {
-//                 var msg =
-//                     "The CORS policy for this site does not " +
-//                     "allow access from the specified Origin.";
-//                 return callback(new Error(msg), false);
-//             }
-//             return callback(null, true);
-//         }
-//     })
-// );
