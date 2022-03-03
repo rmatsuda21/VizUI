bef9fc1d1aba0957a29e63b7e9cea436cf4513b2
jyuan1999 <jyuan1999@tamu.edu>
2022-03-02 18:38:39 -0600

    skeleton button database functionality

--- client/src/widgets/components/MyButton.jsx
+++ client/src/widgets/components/MyButton.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import '../stylesheets/MyButton.css'
+import React, { useState }  from 'react'
 
 function MyButton(props) {
+  var [count, SetCount] = useState();
+
+  const countUpdate=(event)=>{ // Dealing with name field changes to update our state
+    setCount(event.target.value)
+  }
+
+  function PostRequest(props) {
+       
+      fetch('/clicked', {method: 'POST'})
+        .then(function(response) {
+          if(response.ok) {
+            console.log('Click was recorded');
+            return;
+          }
+          throw new Error('Request failed.');
+        })
+        .catch(function(error) {
+          console.log(error);
+      });
+
+
+  }
   return (
     <>
-    <input type="button" value={props.label} />
+    {/* onclick is a listener */}
+    <input type="button" onClick={() => {
+      countUpdate();
+      PostRequest();
+    }} value={props.label} />
+    
     </>
   );
 };
