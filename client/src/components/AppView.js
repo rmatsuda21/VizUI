b971e93e12bb216c153a2029bca91260e72d64f1
Reo Matsuda <reo.matsuda@gmail.com>
2022-03-05 01:26:46 -0600

    Finished upload to app generation flow + refactor some code

--- client/src/components/AppView.js
+++ client/src/components/AppView.js
@@ -1,83 +1,112 @@
-import React, { useEffect } from 'react'
-import MySlider from "../widgets/components/MySlider.jsx"
-import MyButton from "../widgets/components/MyButton.jsx"
+import React, { useEffect, useState } from "react";
+import MySlider from "../widgets/components/MySlider.jsx";
+import MyButton from "../widgets/components/MyButton.jsx";
 import { breadcrumbsClasses } from "@mui/material";
 
 function parseProperties(properties) {
-  var obj = {};
-  console.log(properties)
-  Array.prototype.forEach.call(properties, property => {
-    let key = property["@_name"]
-    console.log("Property type: " + key)
-    switch (key) {
-      case "geometry":
-        obj[key] = property.rect
-        // obj["x"] = property.rect.x
-        // obj["y"] = property.rect.y
-        // obj["width"] = property.rect.width
-        // obj["height"] = property.rect.height
-        break;
-      case "windowTitle":
-        obj[key] = property.string;
-        break;
-      case "singleStep":
-      case "minimum":
-      case "maximum":
-        obj[key] = property.number;
-        break;
-      case "orientation":
-        obj[key] = property.enum;
-        break;
-      default:
-        console.log("New property type: " + key)
-        break;
-    }
-  })
-  return obj;
+    var obj = {};
+    // console.log(properties);
+    Array.prototype.forEach.call(properties, (property) => {
+        let key = property["@_name"];
+        // console.log("Property type: " + key);
+        switch (key) {
+            case "geometry":
+                obj[key] = property.rect;
+                // obj["x"] = property.rect.x
+                // obj["y"] = property.rect.y
+                // obj["width"] = property.rect.width
+                // obj["height"] = property.rect.height
+                break;
+            case "windowTitle":
+                obj[key] = property.string;
+                break;
+            case "singleStep":
+            case "minimum":
+            case "maximum":
+                obj[key] = property.number;
+                break;
+            case "orientation":
+                obj[key] = property.enum;
+                break;
+            default:
+                console.log("New property type: " + key);
+                break;
+        }
+    });
+    return obj;
 }
 
-function App() {
-  // TODO: Add function to render this page dynamically based on JSON
-
-  const data = require("../test.json");
-
-  var widgets = data.ui.widget.widget.map(function(object, i){
-    let name = object["@_name"];
-    let className = object["@_class"];
-    let properties = parseProperties(object.property);
-
+function widgetParser(className, name, properties, i, object) {
     switch (className) {
-      case 'QSlider':
-        let interval = properties.singleStep || 1
-        let min = properties.minimum || 0
-        let max = properties.maximum || 100
-        return <MySlider key={i} name={name} interval={interval} min={min} max={max} position={0}
-                         geometry={properties.geometry ? properties.geometry : undefined}/>
-      case 'QPushButton':
-        return <MyButton key={i} label={name} name={name} 
-                         geometry={properties.geometry ? properties.geometry : undefined}></MyButton>
-      default:
-        return <p key={i}>{object["@_class"]}</p>
+        case "QSlider":
+            let interval = properties.singleStep || 1;
+            let min = properties.minimum || 0;
+            let max = properties.maximum || 100;
+            return (
+                <MySlider
+                    key={i}
+                    name={name}
+                    interval={interval}
+                    min={min}
+                    max={max}
+                    position={0}
+                    geometry={
+                        properties.geometry ? properties.geometry : undefined
+                    }
+                />
+            );
+        case "QPushButton":
+            return (
+                <MyButton
+                    key={i}
+                    label={name}
+                    name={name}
+                    geometry={
+                        properties.geometry ? properties.geometry : undefined
+                    }
+                ></MyButton>
+            );
+        default:
+            return <p key={i}>{object["@_class"]}</p>;
     }
-  })
+}
 
-  let pageProps = parseProperties(data.ui.widget.property)
-  let title = pageProps.windowTitle || "Example App"
-  let geometry = pageProps.geometry
+function App(props) {
+    // TODO: Add function to render this page dynamically based on JSON
+    const [data, setData] = useState(null)
 
-  console.log(pageProps)
+    var widgets =
+        data ?
+        data.ui.widget.widget.map((object, i) => {
+            let name = object["@_name"];
+            let className = object["@_class"];
+            let properties = parseProperties(object.property);
 
-  useEffect(() => {
-    document.title = title
-  }, [])
+            return widgetParser(className, name, properties, i, object);
+        }) : null;
 
-  return (
+    // let pageProps = parseProperties(data.ui.widget.property);
+    // let title = pageProps.windowTitle || "Example App";
+    // let geometry = pageProps.geometry;
 
-    <div className="App">
-      <header><h1>Generated App: {title}</h1></header>
-      {widgets}
-    </div>
-  );
+    // console.log(pageProps);
+
+    // useEffect(() => {
+    //     document.title = title;
+    // }, []);
+
+    useEffect(async () => {
+        // document.title = title;
+        await new Promise(r => setTimeout(r, 500));
+        await fetch(`/api/get-json/${props.id}`).then((data) => data.json()).then((data) => setData(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <header>{/* <h1>Generated App: {title}</h1> */}</header>
+            {widgets || []}
+        </div>
+    );
 }
 
 export default App;
