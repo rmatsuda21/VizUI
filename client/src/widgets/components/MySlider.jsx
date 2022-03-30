30b5c20a421f3f8806a204b38b186f347dda504d
jyuan1999 <jyuan1999@tamu.edu>
2022-03-29 19:22:56 -0500

    sliders

--- client/src/widgets/components/MySlider.jsx
+++ client/src/widgets/components/MySlider.jsx
@@ -5,6 +5,7 @@ import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
 
 function MySlider(props) {
+    const [value, setValue] = useState(props.position);
     let sliderStyle = {
         margin: '10px 5px 0px 5px'
     };
@@ -12,6 +13,59 @@ function MySlider(props) {
     // props.orientation === 'horizontal'
     //     ? sliderStyle = Object.assign(sliderStyle, {width: props.geometry.width}) 
     //     : sliderStyle = Object.assign(sliderStyle, {height: props.geometry.height});
+
+    //db connection test-------------------------------------
+    //creates a new DB entry for each time the slider is changed, allowing user to keep track of historical changes
+    // These methods will update the state properties.
+    function updatePos(e) {
+        set.value(e.target.value)
+    }
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPosition = { ...value };
+
+    // This function will handle the submission.
+    async function onSubmit(e) {
+        e.preventDefault();
+    
+        // When a post request is sent to the create url, we'll add a new record to the database.
+        const newPosition = { data : value };
+    
+        await fetch("/dbwrite/:db_name/:collection_name", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPosition),
+        })
+        .catch(error => {
+        window.alert(error);
+        return;
+        });
+    
+
+    }
+ //end db connection test--------------------------------------------
+
+    const valueUpdate = (position) => {
+        setValue(position);
+        props.onChange(position)
+        console.log(position)
+    };
+
+    function PostRequest() {
+        fetch("/changed", { method: "POST" })
+            .then(function (response) {
+                if (response.ok) {
+                    console.log("Slider was changed");
+                    return;
+                }
+                throw new Error("Slider request failed.");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <>
@@ -26,15 +80,15 @@ function MySlider(props) {
                     {props.name}
                 </Typography>
                 <Slider
-                    aria-label = {props.name} 
-                    style = {sliderStyle}
-                    min = {props.min}
-                    max = {props.max} 
-                    orientation = {props.orientation} 
-                    defaultValue = {props.position}
-                    step = {props.interval} 
-                    valueLabelDisplay = "auto"
-                    marks = {props.marks}
+                    defaultValue={props.position}
+                    step={props.interval}
+                    min={props.min}
+                    max={props.max}
+                    style={sliderStyle}
+                    value = {position}
+                    onChange = {updatePos}
+                    onChangeCommitted = {onSubmit}
+                    valueLabelDisplay="on"
                     sx={{
                         color: '#848ccf', 
                         '& .MuiSlider-track': {
