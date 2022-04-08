ea202c5404ece98984bc537f9b3f556fd4128db3
Reo Matsuda <reo.matsuda@gmail.com>
2022-04-07 20:17:34 -0500

    Tables ez

--- client/src/components/TestTable.jsx
+++ client/src/components/TestTable.jsx
@@ -1,62 +1,155 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { DataGrid, useGridApiContext } from '@mui/x-data-grid';
+import {
+  randomCreatedDate,
+  randomTraderName,
+  randomUpdatedDate,
+} from '@mui/x-data-grid-generator';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
+function EditToolbar(props) {
+  const apiRef = useGridApiContext();
+  const { selectedCellParams, setSelectedCellParams } = props;
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+  const handleClick = async () => {
+    if (!selectedCellParams) {
+      return;
+    }
+    const { id, field, cellMode } = selectedCellParams;
+    if (cellMode === 'edit') {
+      apiRef.current.stopRowEditMode({ id });
+      setSelectedCellParams({ ...selectedCellParams, cellMode: 'view' });
+    } else {
+      apiRef.current.startRowEditMode({ id });
+      setSelectedCellParams({ ...selectedCellParams, cellMode: 'edit' });
+    }
+  };
 
-export default function DataGridDemo() {
+  const handleMouseDown = (event) => {
+    // Keep the focus in the cell
+    event.preventDefault();
+  };
+
+  return (
+    <Box
+      sx={{
+        justifyContent: 'center',
+        display: 'flex',
+        borderBottom: 1,
+        borderColor: 'divider',
+      }}
+    >
+      <Button
+        onClick={handleClick}
+        onMouseDown={handleMouseDown}
+        disabled={!selectedCellParams}
+        color="primary"
+      >
+        {selectedCellParams?.cellMode === 'edit' ? 'Save' : 'Edit'}
+      </Button>
+    </Box>
+  );
+}
+
+EditToolbar.propTypes = {
+  selectedCellParams: PropTypes.any,
+  setSelectedCellParams: PropTypes.func.isRequired,
+};
+
+export default function StartEditButtonGrid() {
+  const [selectedCellParams, setSelectedCellParams] = React.useState(null);
+
+  const handleCellClick = React.useCallback((params) => {
+    setSelectedCellParams(params);
+  }, []);
+
+  const handleCellEditStart = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleCellEditStop = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        sx={{height:'500px', color:"primary.contrastText"}}
-        checkboxSelection
-        disableSelectionOnClick
+        onCellClick={handleCellClick}
+        onCellEditStart={handleCellEditStart}
+        onCellEditStop={handleCellEditStop}
+        components={{
+          Toolbar: EditToolbar,
+        }}
+        componentsProps={{
+          toolbar: {
+            selectedCellParams,
+            setSelectedCellParams,
+          },
+        }}
+        experimentalFeatures={{ newEditingApi: true }}
+        sx={{height:500}}
       />
     </div>
   );
 }
+
+const columns = [
+  { field: 'name', headerName: 'Name', width: 180, editable: true },
+  { field: 'age', headerName: 'Age', type: 'number', editable: true },
+  {
+    field: 'dateCreated',
+    headerName: 'Date Created',
+    type: 'date',
+    width: 180,
+    editable: true,
+  },
+  {
+    field: 'lastLogin',
+    headerName: 'Last Login',
+    type: 'dateTime',
+    width: 220,
+    editable: true,
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    name: randomTraderName(),
+    age: 25,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+  },
+  {
+    id: 2,
+    name: randomTraderName(),
+    age: 36,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+  },
+  {
+    id: 3,
+    name: randomTraderName(),
+    age: 19,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+  },
+  {
+    id: 4,
+    name: randomTraderName(),
+    age: 28,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+  },
+  {
+    id: 5,
+    name: randomTraderName(),
+    age: 23,
+    dateCreated: randomCreatedDate(),
+    lastLogin: randomUpdatedDate(),
+  },
+];
