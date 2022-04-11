d280161b1e6fc6e88b6c0b16bb5d51dcaf541869
jyuan1999 <jyuan1999@tamu.edu>
2022-04-11 04:20:02 -0500

    checked box connection

--- client/src/widgets/components/MyCheckbox.jsx
+++ client/src/widgets/components/MyCheckbox.jsx
@@ -4,14 +4,22 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import { Checkbox } from "@mui/material";
 import { Box } from "@mui/system";
 
+import { useContext } from "react";
+import WidgetContext from "../contexts/WidgetContext";
+
 function MyCheckbox(props) {
-    console.log(props.disabled);
-    const [checked, setchecked] = React.useState(0);
+    // console.log(props.disabled);
+    const {widgetVal, socket, appId} = useContext(WidgetContext);
+    const [checked, setchecked] = React.useState(widgetVal[props.name] ? widgetVal[props.name] : false);
+
+    function toggle(value){
+        return !value;
+      }
 
     async function onSubmit() {
-        // const socket = io();
-        // socket.emit("updateCheckboxValue", value);
-        // console.log("socket emit: updated Dial val to ", value)
+        const checkbox = {appId: appId, data: checked, name: props.name}
+        console.log("client side emit: ", checkbox)
+        socket.emit("widget", checkbox);
     }
 
     return (
@@ -29,8 +37,11 @@ function MyCheckbox(props) {
                     control={
                         <Checkbox
                             value={props.label}
+                            checked={checked}
                             onChange={(e) => {
                                 //socket emit to update checked boxes
+                                console.log(e.target.checked);
+                                setchecked(toggle)
                                 onSubmit();
                             }}
                             defaultChecked={props.defaultChecked}
